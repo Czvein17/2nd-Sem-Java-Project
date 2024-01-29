cba453a1fa029f225ec84ae2df1f7531cf114187
Czvein <czveinlei17@gmail.com>
2024-01-30 00:52:25 +0800

    Create javaproject.sql
    added database file

---
+++ src/main/java/com/javaproject/Assets/database file/javaproject.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: javaproject
+-- ------------------------------------------------------
+-- Server version	8.0.35
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'1','1','1','1','admin',_binary '‰PNG\r\n\Z\n\0\0\0\rIHDR\0\0\0d\0\0\0d\0\0\0p\â•T\0\0\0	pHYs\0\0\0\0\0šœ\0\0ðIDATxœ\íkˆUU\Ç\Ïô²¨>D\ï•E”Ú‹>FEEö°I½{\ß4Š†Ê™»Ö¹ZÕ‡’ü\"E‘Dô ÂŠ ö4%¡2)°4{Ø‡òQ”\ÎY\ëš;Ö™1g†\Ä{\Î=\ç\ì}X¢—µ\Ö\ï\ìóX{\í½=¯R¥J•*UªT©R	\Õ\ÝmöW\Í`R‚²¯€ŸVHŸj\ä5\Zx½ÚªhÄ¶†‡¼FþB^*ÿGþo‚³\å·lÇ“K\Õü¡‰\Ú\ç¹\nø%´E#›D\è/ô®ök>_88hö³«³ªõ\ï8E¥¿OÀ>ñO\nè¡ºœi;~\'4w®9P#õ( ™AÀÿ7¹\Å\ÕT¿d\Ð\à•MÝƒ\æ ¡‘\×\Ù¡ÇƒþA!\ß\Ñ}—9\Ä+¾L—Bš­~µx½o0¿( ^ñ\Ù+¢z\è\\\rô‰\íD\ëè·²kH\çxEQ_Ÿ™ k –\í\ä\êøPX!-’[­—û7\'¤\Ïl\'T\'eÀ«{†N÷ò(\Ý®?\Ôl\'“†B\Ö|š\î\åI\ni¡õ\Äaº¦€\ïñrQ\æ@z\Ìv²tfP\èQg¿öÃ‡7òË¶“¤³·{\Ø\ËU2\\w²žce¤ ¿\æT\áR!-±m\n\Ð\ãž\Ò@÷\ÙN†v\Ç\î¶\nCAp“I0\îí’¹+0\äI#ýa?	\ì”)¤mº\è4\å^\ípR\rô¶\ÜN\Ð\Í\n[—J-mú‚\'\×|s\ÄðŸC5\Òy2Q¥±u™jS5ð\í\Z\éÁÎ‹Ÿ´*\Ó7¯°6\å\"\à/5\Ð-Ýƒ\æ°N\âSH$pQ,ò2¬\Ú:U(T@›er)©´„€°n\Ò/]™.\×J\è\n\éóžù\ÛOL2\Ê$€„ôaªó)\á\ä’[#c\Å\ì†9<…8“‚ld\äzi¨w\Ð¬‘7¹32ø\ÇYMst\Z±&	D\Z)RyÀk\ä[mC\Ðc\Þ÷[Wz))Q È¦4\'ñ\î…¼Á>\Þ}Õ½ê¥¨¤Hó^¢\Ý,#\"\Æ«#]\à\å	\ÈðË‡N\ÌA§¦aW{)+\r òvšˆs\Ò\ÕgŽ2Ÿ±“x\äE \Ö\ä‹\ê~\ëò½™Bþ*y\ßiW\"óñ\n\è\ëp\Õ\èü8qÈ·Š\Ì[h¤¶|—©\íq˜.\é\ä³\rA\ï	\è\ï8_\ã3\Z\ÛOp\á•]ú–;úP”\â›\í ôè€€VÆŒ\ãMÛ¾\ï¶ É±(\à;m Gðó±jo.]T\È}±h\äen¡\ÅyŸ\ÑTÈ¯t\ÒÒ³Í©`€\Ä\0²Ü¶\ßc¶ÄªJ\Ë22û\ÎóXˆÄ‡ùx›\ÙÎŠƒl;®¸ÿ\Ú|\ÍÝ›\Õ0¸.öó¤w¸Bmœ3`ˆD#?“w º\ß\ê(§\"‘…*\ÖÇ¢‘™\Ä\È@x­uÇ±˜@¤VˆF\Þh\Ûq]\\ b\0IpÁ~ÄŒ\ÍýˆlU\á\0]\ÈB³@¤s^º	Û±¾>3!Z¦«\Ý\ßm©W(€(L\Öf\Ö\á×¦t¿9&õ˜6Gwù\ç2Q\r¾8ƒ¸7FvL[F 5 9\é\Ç\Ìk#;&/e¢³h$Z\Ù1\Ùü«”@0ƒ\ÙE\à\'#;&se¢€¿K?fö#;&»”\rH·l•ÁRY\n\Ù9Ù£°l@z€&;;A5\ÜD¿•	ˆ\Æ`Z6e“˜­@Y49¸D!\ÏwºQ\\!72\Â\Ë\ê>u·c\Ú:#Šÿ\Òq\Þ\îo‡¿ô^\ê ð<\ç\ï©e*.ªf0\É\ëD\nù\ÛA\è‚\0QÀ_w#\n\é~Û\è¢\0A¾·s Mšb;] Þ®þ\é#\ÛÁ\èœ‘E@^\Ñ6™Q9\"{P&d¸\Ï\×þ¢O•S ’»\Ä78“d8X_.4ø6/i…K£3¨„\ê¢^Ÿ\Ú\Î@YU€u‘€`0\ÍKOaÁqe„Û»€€V¤¾™¿”\å\ÐP5Bx£™‚\Ì6ñ—\Í+ œü¬`\\I\Õ\Æ*7\ÏZ•ùI=½8tªL¶T@x<Œ-\ÖNPm(²\\2¦œ!´³­«\Ò\Ëx;AûŒŽ½(5)¤‡\Ë>B\Ò#ž;\n÷Dy¢¬@òRçŽ­)@>W6 \nøY\ç`Œ;oaY€(¤%\Ã\Ø#Iœú§¸@h§lK\î\åIu¿uu\Ò\ß)\Ê ²\ØFa\ë\Z/š\Ñ4\Ç*¤w\nh¹‚\í\'yyVø°^DARY\"…B‰Á©c:\Õ\Èño\å\rˆz_*\Ü^Q%\rq›\ïT¶@\ÖÉŽH^$¯Š\Zƒk5ò®Q\á–\"\ÔS\Ês\Õ\Ã\ãö\ZÁTüz;EJ•p‘½!¾ö¸\îXû\ê/žg¡]Y\0	O\äñ\Õ|s|z‘@½\æ89«d\ä°Ê	Ù¤_\Ð@³d/\ßô\"(¸\ê¾9Rù­+¢\ÎQKûM\r¹_\æmf\Î3G¥\ça¥J•*UªT©R%\Ïiý®\å\×\Ç\ï²&\0\0\0\0IEND®B`‚'),(2,'manager','manager','manager','2','manager',NULL),(3,'useradmin','user@gmail.com','userako#20223','','user',_binary '‰PNG\r\n\Z\n\0\0\0\rIHDR\0\0\0\0\0\0\0\0\0\àw=ø\0\0vIDATxœµ•Mn\Ú@\Çß³a—7(Dê®°8A\Ò\0\î	\n\'=A\É	j\à\0MN€°\ÝUŠ‘\È	Š\Ô\îŒýúcóÆ˜Eû“¬ù\Ï\×û`\ÞLÿ­ƒb­y\ãNGÈ‹I\Ú\Ãøv+ÍŽ}…T3kƒ©û2\Ùt†\ëZ\Ó\n{„r\èbu\Ýù¨‚þ–\ëºU\n<ÿrœd3fù§c¯H\Ãûº•÷6\Ø1²fùÅ±0Q¬¶z\"ry³\Ùpg¶C\ZŠU«.\â!`\æ/\îl\Ø#°s¦\Z\Ð7\È#£y‹6k`ô)þiw8ƒ…}€Œ\àÁr>´\è…J\Ë&’6E0\ÑœA	2ý=¥º•û\ãù–0\ç\r–EE\Z¬£ö°\È\ê*k\Ú\Ç^c8„ñ\í@4mb¹\Ã`S‘1Iúýc\Ô9ø·°T¢5	?\"\ë¦B_y0’ºG`A)÷!O#\Ëú\0&^\\e†\Êû\Õ\ÂÖ³\Ö82\Õ	¨HQ1mHŒ›ƒ8³bµ\éˆ\Ð\r\äÊ‰;–9)\n5”¡BÍº£ÀÿŽ¡=†ùq9µµsªš8)\År>b4\Ú(™igW¨4\Í	Œ59€½\Ãw\È+\æó0/hN¨\ë¾\Ùiˆoc¡\Ú\ê“\Èg\È=\Ì\ËÙ°…[=Ò\Éva\Z\Û*óm„y‹.a\Ó\ê\ÝRU‚\îY°örŠµV\èÀŸ\Ð\\Dªƒ(ú{a²(~5ñÊ¢ûW\ã!-‹³¶\ÆõP\'»…®–³ðt\Øtði98l‰Tô¿=ÿd*(\×D;‰\Ñ\Ó0†L%¾ñ¤!Ñºž\ç;tzÁ\ÞòšÍšõ¤¿\ÕDÿŠ¿ )\Ñ|!À\0\0\0\0IEND®B`‚'),(4,'lei3','lei4@gmail.com','NIEVZCvein#17','','user',NULL),(5,'55','50','userAdmin','2','user',NULL);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-01-30  0:49:46
